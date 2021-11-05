4874fcd31d7cffaeaabdaec81bf5256ba1890445
Sebastian Huebner <sebastian@hueb-ner.de>
2021-11-05 20:36:30 +0100

    changed local build script for lua

--- lua/update.sh
+++ lua/update.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
+
 set -eo pipefail
 
 docker pull alpine:latest
+docker pull ubuntu:latest
 
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-versions=( */ )
-for version in "${versions[@]%/}"; do
-    image=imolein/lua:${version}
-    echo building "${image}" ...
-    docker build -q -t "${image}" "${version}"
-    docker run --rm "${image}" lua -v
+lua_versions=( */ )
+for lua_version in "${lua_versions[@]%/}"; do
+    image="imolein/lua:${lua_version}"
+
+    for dockerfile in ${lua_version}/*; do
+        version="${dockerfile##*.}"
+
+        [ "${version}" != "alpine" ] && image="${image}-${version}"
+
+        echo "building '${image}' ..."
+        docker build -f "${dockerfile}" -t "${image}" "${lua_version}/"
+        docker run --rm "${image}" lua -v
+    done
 done
